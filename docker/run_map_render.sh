e91b02aebedfb868f224eb3c844e0043ca3c13dd
freshmeat <fernandesvalentin1235@gmail.com>
2021-12-16 12:58:14 +0100

    change render config

--- docker/run_map_render.sh
+++ docker/run_map_render.sh
@@ -4,6 +4,7 @@ green='\e[1;32m%s\e[0m\n'
 yellow='\e[1;33m%s\e[0m\n'
 printf "$yellow" " ###### Minecraft Server - RENDER PROCESS ######"
 cd ~/MyMinecraft 
+cp /render/config.py ~/MyMinecraft/minecraft-data
 if [ -f .env ]; then
   # Load Environment Variables
   export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
@@ -11,9 +12,11 @@ if [ -f .env ]; then
   --rm \
   -e MINECRAFT_VERSION=${MINECRAFT_VERSION} \
   -e ADDITIONAL_ARGS="--no-tile-checks" \
+  -e CONFIG_LOCATION="/home/minecraft/server/config.py"
   -v ~/MyMinecraft/minecraft-data:/home/minecraft/server/:ro \
   -v ~/MyMinecraft/mapOverview:/home/minecraft/render/:rw \
   -m 6G \
   mide/minecraft-overviewer:latest
   printf "$green" "Map render finished!"
 fi
+rm ~/MyMinecraft/minecraft-data/config.py
---
+++ render/config.py
@@ -0,0 +1,87 @@
+# This config variable is loaded into the upstream Minecraft Overviewer project,
+# so it contains undefined variables and some `import` lines.
+# flake8: noqa: F821,F401
+# pylint: disable=undefined-variable
+# type: ignore
+
+import os
+
+
+def playerIcons(poi):
+    if poi["id"] == "Player":
+        poi["icon"] = "https://overviewer.org/avatar/{}".format(poi["EntityId"])
+        return "Last known location for {}".format(poi["EntityId"])
+
+
+# Only render the signs with the filter string in them. If filter string is
+# blank or unset, render all signs. Lines are joined with a configurable string.
+def signFilter(poi):
+    # Because of how Overviewer reads this file, we must "import os" again here.
+    import os
+
+    # Only render signs with this function
+    if poi["id"] in ["Sign", "minecraft:sign"]:
+        sign_filter = os.environ["RENDER_SIGNS_FILTER"]
+        hide_filter = os.environ["RENDER_SIGNS_HIDE_FILTER"] == "true"
+        # Transform the lines into an array and strip whitespace from each line.
+        lines = list(
+            map(
+                lambda l: l.strip(),
+                [
+                    poi["Text1"],
+                    poi["Text2"],
+                    poi["Text3"],
+                    poi["Text4"],
+                ],
+            )
+        )
+        # Remove all leading and trailing empty lines
+        while lines and not lines[0]:
+            del lines[0]
+        while lines and not lines[-1]:
+            del lines[-1]
+        # Determine if we should render this sign
+        render_all_signs = len(sign_filter) == 0
+        render_this_sign = sign_filter in lines
+        if render_all_signs or render_this_sign:
+            # If the user wants to strip the filter string, we do that here. Only
+            # do this if sign_filter isn't blank.
+            if hide_filter and not render_all_signs:
+                lines = list(filter(lambda l: l != sign_filter, lines))
+            return os.environ["RENDER_SIGNS_JOINER"].join(lines)
+
+
+worlds["minecraft"] = "/home/minecraft/server/world"
+outputdir = "/home/minecraft/render/"
+
+markers = [
+    dict(name="Players", filterFunction=playerIcons),
+    dict(name="Signs", filterFunction=signFilter),
+]
+
+renders["day"] = {
+    "title": "Day",
+    "dimension": "overworld",
+    "markers": markers,
+    "rendermode": "lighting",
+    "world": "minecraft",
+    "northdirection":"lower-right",
+}
+
+renders["nether"] = {
+    "title": "Nether",
+    "dimension": "nether",
+    "markers": markers,
+    "rendermode": "nether_lighting",
+    "world": "minecraft",
+    "northdirection":"lower-right",
+}
+
+renders["end"] = {
+    "title": "End",
+    "dimension": "end",
+    "markers": markers,
+    "rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.5)],
+    "world": "minecraft",
+    "northdirection":"lower-right",
+}
